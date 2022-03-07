anirudhkosgi/device_xiaomi_laurel_sprout

laurel_sprout: Build with GrapheneOS Camera

GrapheneOSCamera/Android.mk
@@ -1,0 +1,9 @@
+include $(CLEAR_VARS)
+LOCAL_MODULE := GrapheneOSCamera
+LOCAL_SRC_FILES := GrapheneOSCamera/GrapheneOSCamera.apk
+LOCAL_MODULE_CLASS := APPS
+LOCAL_PRIVILEGED_MODULE := true
+LOCAL_OVERRIDES_PACKAGES := Snap Camera2
+LOCAL_CERTIFICATE := PRESIGNED
+LOCAL_DEX_PREOPT := false
+include $(BUILD_PREBUILT)

aliothtrees/device_xiaomi_sm8250

Camera/Android.mk
@@ -1,0 +1,9 @@
+include $(CLEAR_VARS)
+LOCAL_MODULE := Camera
+LOCAL_SRC_FILES := Camera/Camera.apk
+LOCAL_MODULE_CLASS := APPS
+LOCAL_PRIVILEGED_MODULE := true
+LOCAL_OVERRIDES_PACKAGES := Snap Camera2
+LOCAL_CERTIFICATE := PRESIGNED
+LOCAL_DEX_PREOPT := false
+include $(BUILD_PREBUILT)